4d5478f5e4ead8925718703a779b796695ba0079
Essi Purhonen <pessiapps@gmail.com>
2024-07-24 12:14:33 +0300

    fiksattu ikuinen loop notifikation reducerissa

--- redux-anecdotes/src/reducers/notificationReducer.js
+++ redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,20 +1,26 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const notificationSlice = createSlice({
-  name: 'notification',
-  initialState: '',
+  name: "notification",
+  initialState: "",
   reducers: {
     setNotificationMessage(state, action) {
-      return action.payload
+      return action.payload;
     },
-  }
-})
+  },
+});
 
-export const { setNotificationMessage } = notificationSlice.actions
-export const setNotification = ( message, timeSeconds) => {
-  return dispatch => {
-    dispatch(setNotificationMessage(message))
-    setTimeout(() => { dispatch(setNotification('')) }, timeSeconds*1000)
-  }
-}
-export default notificationSlice.reducer
+export const { setNotificationMessage } = notificationSlice.actions;
+export const setNotification = (message, timeSeconds) => {
+  console.log("täällä11111");
+
+  return (dispatch) => {
+    console.log("tääällä2222");
+
+    dispatch(setNotificationMessage(message));
+    setTimeout(() => {
+      dispatch(setNotificationMessage(""));
+    }, timeSeconds * 1000);
+  };
+};
+export default notificationSlice.reducer;
